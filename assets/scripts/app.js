f59643bd57c2f5361ebcaa15ac1b74403056258f
shantalanarayan <mail2shantala@gmail.com>
2020-01-07 15:17:02 -0500

    added event to the grid, fixed code.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const events = require('./events')
-const authEvents = require('./auth/events')
+const authEvents = require('./auth/events');
 
-$(() => {
+(() => {
   events.toggleForm()
   authEvents.addHandlers()
 
